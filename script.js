74f4ba234e1190b14611627431e96ffebea03e6c
Thomastw374 <thomas.whiteley46@gmail.com>
2022-07-01 12:29:05 +0100

    Buttons now output to calculator screen

---
+++ script.js
@@ -0,0 +1,58 @@
+const keypadNumber = document.querySelectorAll(".main__calculator-keypad-number")
+const keypadEquals = document.querySelector("#keypad-equals")
+
+console.dir(keypadNumber)
+
+const calculatorText = document.querySelector(".main__calculation-textbox");
+
+console.dir(calculatorText)
+
+
+keypadNumber.forEach(item => {item.addEventListener('click', event =>{
+    calculatorText.value += item.innerText
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const equation = "42 * 5"
+
+// equationArr = equation.split(" ")
+
+// console.log(equationArr)
+
+// let answer = 0
+
+// for(i=0; i<equationArr.length; i++) {
+//     if (equationArr[i] = "*") {
+//         let answer = Number(equationArr[i-1])*Number(equationArr[i+1])
+//         return answer
+//     }
+//     console.log(answer)
+// }
+
+// console.log(answer)
+
+
+// essentially need to write a parser
+
+// could make the calculator include spaces before and after operators. Then split at spaces, forming an array of alternating number and operator strings.
+
+// I can then create a for loop that iterates through the array and performs operation accordingly.
+
+// store first element in var1, check second element, store in var2, store third element in var3. If var2 = *, var1*var3 etc. You could have a runningtotalvar just like reduce! In order to perform an equation we must have all the currently relevant numbers and operators stored. It would be difficult to implement order of operations going step by step. You would have to have all the operators and numbers stored. Could try to implement order of operations, maybe no include brackets or exponentation. so dmas
+
+// How to implement order of operations?
+
+// Could use an indexof for loop to find the index of / and * operators. Then we can
