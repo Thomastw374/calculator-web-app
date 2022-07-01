6a85cb947cee15e4fb25559f78788b5ddfd19abc
Thomastw374 <thomas.whiteley46@gmail.com>
2022-07-01 16:46:19 +0100

    Adding a third operator is now possible

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 const keypadNumber = document.querySelectorAll(".main__calculator-keypad-number")
 const keypadOperator = document.querySelectorAll(".main__calculator-keypad-operator")
 const keypadEquals = document.querySelector("#keypad-equals")
-
 const calculatorText = document.querySelector(".main__calculation-textbox");
-
+const keypadClear = document.querySelector("#keypad-clear")
 // let equationStringArr = []
 let var1 = ""
 let var2 = ""
 let var3 = ""
 let answer
 
-
+function evaluateFunction() {
+    if (var2 == "*") {
+      calculatorText.value = var1 * var3;
+    } else if (var2 == "/") {
+      calculatorText.value = var1 / var3;
+    } else if (var2 == "-") {
+      calculatorText.value = var1 - var3;
+    } else if (var2 == "+") {
+      calculatorText.value = var1 + var3;
+    } 
+    var2 = ""
+}
 
 keypadNumber.forEach(item => {item.addEventListener('click', event =>{
     calculatorText.value += item.innerText
@@ -21,6 +31,9 @@ keypadNumber.forEach(item => {item.addEventListener('click', event =>{
 
 keypadOperator.forEach((item) => {
   item.addEventListener("click", (event) => {
+    if (var2 !== "") {
+        evaluateFunction()
+    }
     var2 = item.innerText
     var1 = Number(calculatorText.value);
     calculatorText.value += item.innerText;
@@ -32,20 +45,19 @@ const handleEqualsPress = (event) => {
     const equationArr = calculatorText.value.split(/[-/*+-]+/) 
     var3 = Number(equationArr[1])
     console.log(var1,var2,var3)
-    if (var2 == '*') {
-        calculatorText.value = var1*var3
-    } else if (var2  == '/') {
-        calculatorText.value = var1/var3;
-    } else if (var2  == '-') {
-        calculatorText.value = var1 - var3;
-    } else if (var2  == '+') {
-        calculatorText.value = var1 + var3;
-    } 
+    evaluateFunction()
     console.log(var2)
+}
+
+const handleKeypadClearPress = (event) => {
+    calculatorText.value = ""
 }
 
 
 keypadEquals.addEventListener("click", handleEqualsPress)
+keypadClear.addEventListener("click", handleKeypadClearPress)
+
+
 
 
 
