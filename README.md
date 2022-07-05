c4d70979da98f4d7923d8866bb04595d00cd9760
Thomastw374 <thomas.whiteley46@gmail.com>
2022-07-05 15:08:10 +0100

    updated readme and commented js

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 The enclosed project a constitutes a simple calculator. Able to perform the 4 basic operations, multiplication, divison, addition and subtraction.
 
-This was created without the use of the eval() function due to it's inherent security flaws, for one potentially allowing for code injection attacks. Function() could have similarly been used here but I decided to opt for a more roundabout method to test my basic javascript skills.
+This was created without the use of the eval() function due to it's inherent security flaws, potentially allowing for code injection attacks. Similarly, Function() could have been used here but I decided to opt for a more roundabout method to test my basic javascript skills.
--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const keypadOperator = document.querySelectorAll(".main__calculator-keypad-opera
 const keypadEquals = document.querySelector("#keypad-equals")
 const calculatorText = document.querySelector(".main__calculation-textbox");
 const keypadClear = document.querySelector("#keypad-clear")
-// let equationStringArr = []
+// Created 3 variables. They represent the first number, operator and second number.
 let var1 = ""
 let var2 = ""
 let var3 = ""
 let answer
-
+// A function that performs the relevant operation and outputs it to the calculator screen.
 function evaluateFunction() {
     if (var2 == "*") {
       calculatorText.value = var1 * var3;
@@ -21,7 +21,8 @@ function evaluateFunction() {
     } 
     var2 = ""
 }
-
+// Outputs a buttons number to the screen upon pressing.
+// In the event that an operator is already present it stores the second number in var3.
 keypadNumber.forEach(item => {item.addEventListener('click', event =>{
     calculatorText.value += item.innerText
     if (var2 !== "") {
@@ -30,7 +31,8 @@ keypadNumber.forEach(item => {item.addEventListener('click', event =>{
     }
     })
 })
-
+// If an operator is already present this function will call the evaluate function
+// It also stores the selected operator in var2 and stores the first number in var1.
 keypadOperator.forEach((item) => {
   item.addEventListener("click", (event) => {
     if (var3 !== "") {
@@ -39,25 +41,21 @@ keypadOperator.forEach((item) => {
     var2 = item.innerText
     var1 = Number(calculatorText.value);
     calculatorText.value += item.innerText; 
-    console.log(var1,var2,var3)
   });
 });
-
+// Stores second number in var3 and calls the evaluate function
 const handleEqualsPress = (event) => {
     const equationArr = calculatorText.value.split(/[-/*+-]+/) 
     var3 = Number(equationArr[1])
-    console.log(var1,var2,var3)
     evaluateFunction()
-    console.log(var2)
 }
-
+// Clears the calculator
 const handleKeypadClearPress = (event) => {
     calculatorText.value = ""
     var3="";
     var2="";
     var1="";
 }
-
-
+// Adding of even listeners
 keypadEquals.addEventListener("click", handleEqualsPress)
 keypadClear.addEventListener("click", handleKeypadClearPress)
